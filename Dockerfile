57976f757e552e8dd89c96708f56eb62e2742c63
Cole Gleason <colewashere@gmail.com>
2014-06-24 14:32:25 -0700

    fixup to work with port 80 on debian with new kibana format

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-FROM base
+FROM debian
 
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y wget nginx-full
 RUN mkdir /usr/share/nginx/www/kibana/
-RUN (cd /tmp && wget --no-check-certificate https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz -O pkg.tar.gz && tar zxf pkg.tar.gz && cd kibana-* && cp -rf ./* /usr/share/nginx/www/kibana/)
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+RUN (cd /tmp && wget --no-check-certificate https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz -O pkg.tar.gz && tar zxf pkg.tar.gz && cd kibana-* && cp -rf ./* /usr/share/nginx/www/)
+RUN rm -rf /tmp/*
+EXPOSE 80
 
-## listen on 8080, not 80
-RUN sed -i  -E 's|#listen.*80;.*|listen 8080;|' /etc/nginx/sites-enabled/default
 ADD run.sh /usr/local/bin/run
 RUN chmod +x /usr/local/bin/run
-RUN rm -rf /tmp/*
 
-EXPOSE 8080
 CMD ["/usr/local/bin/run"]
--- run.sh
+++ run.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-ES_HOST=${ES_HOST:-window.location.hostname}
+ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
 ES_PORT=${ES_PORT:-9200}
 
 cat << EOF > /usr/share/nginx/www/config.js
-var config = new Settings(
-{
-  elasticsearch:    "http://$ES_HOST:$ES_PORT",
-  kibana_index:     "kibana-int",
-  modules:          ['histogram','map','pie','table','stringquery','sort',
-                    'timepicker','text','fields','hits','dashcontrol',
-                    'column','derivequeries','trends','bettermap'],
-  }
-);
+define(['settings'],
+function (Settings) {
+  "use strict";
+  return new Settings({
+    elasticsearch:    "http://$ES_HOST:$ES_PORT",
+    default_route     : '/dashboard/file/default.json',
+    kibana_index: "kibana-int",
+    panel_names: [
+      'histogram',
+      'map',
+      'goal',
+      'table',
+      'filtering',
+      'timepicker',
+      'text',
+      'hits',
+      'column',
+      'trends',
+      'bettermap',
+      'query',
+      'terms',
+      'stats',
+      'sparklines'
+    ]
+  });
+});
 EOF
 
 nginx -c /etc/nginx/nginx.conf
